008e277d33430b00c5186cf7a47719ff60a5d0b1
jdeepee <joshuadparkin@gmail.com>
2022-04-07 11:31:37 +0100

    add basic api

---
+++ queries.js
@@ -0,0 +1,21 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: process.env.user,
+  host: process.env.host,
+  database: process.env.database,
+  password: process.env.password,
+  port: process.env.port,
+})
+
+const dumpLinks = (request, response) => {
+  pool.query('SELECT * FROM linktable', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+module.exports = {
+    dumpLinks
+}
