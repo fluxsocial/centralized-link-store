17f01b418e9bd83768beb1fa8bb8c1e7cbe0cf8e
jdeepee <joshuadparkin@gmail.com>
2022-04-07 14:46:29 +0100

    add logic for querying and adding links

--- index.js
+++ index.js
@@ -20,11 +20,99 @@ app.get('/dump', (request, response) => {
     return db.dumpLinks(request, response)
 })
 
-app.post('/getLinks', (request, response) => {
-    let query = request.body
-    return response.status(200).json(query)
+app.post('/getLinks', async (request, response) => {
+    let query = request.body;
+    if (!query["graph"]) {
+        return response.status(400).json({"error": "Query should have a graph field"})
+    }
+    let links;
+    //source; cases source, source & predicate, source & target
+    if (query["source"] && !query["predicate"] && !query["target"]) {
+        links = await db.queryLinksSource(query["graph"], query["source"])
+    }
+    if (query["source"] && query["predicate"] && !query["target"]) {
+        links = await db.queryLinksSourcePredicate(query["graph"], query["source"], query["predicate"])
+    }
+    if (query["source"] && query["target"] && !query["predicate"]) {
+        links = await db.queryLinksSourceTarget(query["graph"], query["source"], query["target"])
+    }
+
+    //predicate; cases predicate, predicate & target
+    if (query["predicate"] && !query["source"] && !query["target"]) {
+        links = await db.queryLinksPredicate(query["graph"], query["predicate"])
+    }
+    if (query["predicate"] && query["target"] && !query["source"]) {
+        links = await db.queryLinksPredicateTarget(query["graph"], query["predicate"], query["target"])
+    }
+
+    //target; cases target
+    if (query["target"] && !query["source"] && !query["predicate"]) {
+        links = await db.queryLinksTarget(query["graph"], query["target"])
+    }
+    
+    links = links.map((link) => link.link_expression);
+    //filter results with from & until dates
+    function fromDateFilter(link) {
+        const reverse = new Date(query["fromDate"]) >= new Date(query["untilDate"]);
+        if (reverse) {
+            return new Date(link["timestamp"]) <= new Date(query["fromDate"])
+        } else {
+            return new Date(link["timestamp"]) >= new Date(query["fromDate"])
+        }
+    }
+
+    function untilDateFilter(link) {
+        const reverse = new Date(query["fromDate"]) >= new Date(query["untilDate"]);
+        if (reverse) {
+            return new Date(link["timestamp"]) >= new Date(query["untilDate"])
+        } else {
+            return new Date(link["timestamp"]) <= new Date(query["untilDate"])
+        }
+    }
+
+    function limitFilter(results) {
+        const reverse = new Date(query["fromDate"]) >= new Date(query["untilDate"]);
+        if (query["limit"]) {
+            const startLimit = reverse ? results.length - query.limit : 0;
+            const endLimit = reverse ? (results.length - query.limit) + query.limit : query.limit;
+            return results.slice(startLimit, endLimit)
+        }
+
+        return results;
+    }
+
+    if (query["fromDate"]) links = links.filter(fromDateFilter)
+    if (query["untilDate"]) links = links.filter(untilDateFilter)
+    links = limitFilter(links);
+    return response.status(200).json(links)
+})
+
+app.post('/addLink', (request, response) => {
+    let link = request.body;
+    //Check LinkExpression
+    if (!link["data"]) {
+        return response.status(400).json({"error": "LinkExpression should have a data field"})
+    }
+    if (!link["author"]) {
+        return response.status(400).json({"error": "LinkExpression should have an author field"})
+    }
+    if (!link["timestamp"]) {
+        return response.status(400).json({"error": "LinkExpression should have a timestamp field"})
+    }
+    if (!link["graph"]) {
+        return response.status(400).json({"error": "LinkExpression should have a graph field"})
+    }
+    //Check LinkData
+    if (!link["data"]["source"]) {
+        return response.status(400).json({"error": "LinkExpression data should have a source field"})
+    }
+    if (!link["data"]["target"]) {
+        return response.status(400).json({"error": "LinkExpression data should have a target field"})
+    }
+    db.addLink(link["data"]["source"], link["data"]["predicate"], link["data"]["target"], link["graph"], link["timestamp"], link)
+    return response.status(200).json({"status": "Link Added"})
 })
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
+  console.log(`App running on port ${port}.`);
 })
--- queries.js
+++ queries.js
@@ -8,7 +8,7 @@ const pool = new Pool({
 })
 
 const dumpLinks = (request, response) => {
-  pool.query('SELECT * FROM linktable', (error, results) => {
+  pool.query(`SELECT * FROM ${process.env.table}`, (error, results) => {
     if (error) {
       throw error
     }
@@ -16,6 +16,63 @@ const dumpLinks = (request, response) => {
   })
 }
 
+const addLink = (source, predicate, target, graph, timestamp, linkExpression) => {
+  delete linkExpression["graph"];
+  pool.query(`INSERT INTO ${process.env.table}(source, predicate, target, graph, timestamp, link_expression) VALUES ($1, $2, $3, $4, $5, $6)`, 
+    [
+      source, 
+      predicate, 
+      target, 
+      graph, 
+      timestamp,
+      linkExpression
+    ])
+}
+
+// Source queries
+
+const queryLinksSource = async (graph, source) => {
+  const res = await pool.query(`SELECT * FROM ${process.env.table} WHERE source = ($1) AND graph = ($2)`, [source, graph])
+  return res.rows;
+}
+
+const queryLinksSourcePredicate = async (graph, source, predicate) => {
+  const res = await pool.query(`SELECT * FROM ${process.env.table} WHERE source = ($1) AND predicate = ($2) AND graph = ($3)`, [source, predicate, graph]);
+  return res.rows;
+}
+
+const queryLinksSourceTarget = async (graph, source, target) => {
+  const res = await pool.query(`SELECT * FROM ${process.env.table} WHERE source = ($1) AND target = ($2) AND graph = ($3)`, [source, target, graph]);
+  return res.rows;
+}
+
+//Predicate queries
+
+const queryLinksPredicate = async (graph, predicate) => {
+  const res = await pool.query(`SELECT * FROM ${process.env.table} WHERE predicate = ($1) AND graph = ($2)`, [predicate, graph]);
+  return res.rows;
+}
+
+
+const queryLinksPredicateTarget = async (graph, predicate, target) => {
+  const res = await pool.query(`SELECT * FROM ${process.env.table} WHERE predicate = ($1) AND graph = ($2) AND target = ($3)`, [predicate, graph, targtet]);
+  return res.rows;
+}
+
+//Target queries
+
+const queryLinksTarget = async (graph, target) => {
+  const res = await pool.query(`SELECT * FROM ${process.env.table} WHERE target = ($1) AND graph = ($2)`, [target, graph]);
+  return res.rows;
+}
+
 module.exports = {
-    dumpLinks
+  dumpLinks,
+  addLink,
+  queryLinksSource,
+  queryLinksPredicate,
+  queryLinksTarget,
+  queryLinksSourcePredicate,
+  queryLinksSourceTarget,
+  queryLinksPredicateTarget
 }
